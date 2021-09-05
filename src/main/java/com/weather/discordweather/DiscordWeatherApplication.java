ab607231d09c3e900c7d369048fc11900796e585
Christoper Nguyen <junochris93@gmail.com>
2021-09-05 11:24:16 -0700

    Initialized with Spring.

---
+++ src/main/java/com/weather/discordweather/DiscordWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.weather.discordweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weather/discordweather/DiscordWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.discordweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
